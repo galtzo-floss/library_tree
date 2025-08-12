e07550dfecf0719a91a03b9fbd29b97c3471b1d8
Peter H. Boling <peter.boling@gmail.com>
2025-08-11 19:49:40 -0600

    🎉 gem skeleton

---
+++ lib/library_tree.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "library_tree/version"
+
+module LibraryTree
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/library_tree_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LibraryTree do
+  it "has a version number" do
+    expect(LibraryTree::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
